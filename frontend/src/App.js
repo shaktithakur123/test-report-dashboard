11f48b9d8138a0e7d5fec0b8f835437ca0a74819
Shakti Raj <shakti@Shaktis-MacBook-Air.local>
2025-10-19 22:39:58 +0530

    feat: Add basic App component

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Test Report Dashboard</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
+
+export default App;
